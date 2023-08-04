6b74cea0a094bd47fee8b7cd4329140e9607b1c3
Syed Razauddin Shahlal <96375316+syedshahlal@users.noreply.github.com>
2023-08-04 01:14:57 -0400

    Update update_commit.sh

--- update_commit.sh
+++ update_commit.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Get the current date in the format "Month Day, Year"
 current_date=$(date +'%B %d, %Y')
-
-# Replace the placeholder in the README file with the current date
 sed -i "s/{{LAST_COMMIT_DATE}}/$current_date/g" README.md
